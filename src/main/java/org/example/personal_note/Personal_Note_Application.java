bcd60fd7e4f2b0d79650cdecd29e916d41dd5445
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-02-27 23:11:30 +0900

    # 1 feat : first commit description : spring boot 프로젝트 빌드 및 셋팅

---
+++ src/main/java/org/example/personal_note/Personal_Note_Application.java
@@ -0,0 +1,11 @@
+package org.example.personal_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Personal_Note_Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Personal_Note_Application.class, args);
+	}
+}
